de1fa8ed138a8631731eb0b88e7ebfcb55e64546
arvind122-byt <bittuprabhas10@gmail.com>
2025-02-01 17:59:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ count = 5
 ami = "ami-0f214d1b3d031dc53"
 instance_type = "t2.medium"
 key_name = "swiggy"
-vpc_security_group_ids = ["sg-0f67e6d981d627cff"]
+vpc_security_group_ids = ["sg-061d6949815d0c231"]
 tags = {
 Name = var.instance_names[count.index]
 }
